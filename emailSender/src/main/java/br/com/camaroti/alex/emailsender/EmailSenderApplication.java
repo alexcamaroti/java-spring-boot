473d82df5d7572b0ceb500d632b0760f3a821cfe
eltonrufino <eltonrufino@hotmail.com>
2018-09-12 23:10:31 -0300

    ajustes de codigo

--- emailSender/src/main/java/br/com/camaroti/alex/emailsender/EmailSenderApplication.java
+++ emailSender/src/main/java/br/com/camaroti/alex/emailsender/EmailSenderApplication.java
@@ -1,4 +1,4 @@
-package br.com.camaroti.alex.emailSender.emailSender;
+package br.com.camaroti.alex.emailsender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmailSenderApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(EmailSenderApplication.class, args);
 	}
 }
--- emailSender/src/main/java/br/com/camaroti/alex/emailsender/service/EmailSenderService.java
+++ emailSender/src/main/java/br/com/camaroti/alex/emailsender/service/EmailSenderService.java
@@ -1,4 +1,4 @@
-package br.com.camaroti.alex.emailSender.emailSender.service;
+package br.com.camaroti.alex.emailsender.service;
 
 import org.apache.commons.mail.DefaultAuthenticator;
 import org.apache.commons.mail.Email;
@@ -7,23 +7,23 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EmailSenderService {
-	
-	public void emailSender(String name, String emailReceiver) {
+
+	public void emailSender(final String name, final String emailReceiver) {
 		try {
-			Email emailDestiny = new SimpleEmail();
-			
+			final Email emailDestiny = new SimpleEmail();
+
 			emailDestiny.setHostName("smtp.googlemail.com");
 			emailDestiny.setSmtpPort(465);
 			emailDestiny.setAuthenticator(new DefaultAuthenticator("duvalkin.salazar@gmail.com", "11011994"));
 			emailDestiny.setSSLOnConnect(true);
-			
+
 			emailDestiny.setFrom("duvalkin.salazar@gmail.com");
 			emailDestiny.setSubject("A big reward to you");
 			emailDestiny.setMsg("Hello" + name + ". You are invited by VIPList!");
 			emailDestiny.addTo(emailReceiver);
 			emailDestiny.send();
-			
-		} catch (Exception e) {
+
+		} catch (final Exception e) {
 			e.printStackTrace();
 		}
 	}
